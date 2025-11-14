a7317dc3abecf994df21f64a18cf94fc151b38d9
azeezabidoye <azeezabidoye@yahoo.com>
2025-11-14 06:39:35 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Square from "./components/Square";
 import Input from "./components/Input";
 
 const App = () => {
-  const [colorName, setColorName] = useState("Empty value");
+  const [colorName, setColorName] = useState("");
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    const value = e.target.value;
-    setColorName(value.toLowerCase());
-  };
+  // const handleChange = (e) => {
+  //   e.preventDefault();
+  //   const value = e.target.value;
+  //   setColorName(value.toLowerCase());
+  // };
 
   return (
     <div className="App">
